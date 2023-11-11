1a0e8a1f4f6f51256ceba090f1299c7a85f018a9
Evgheni Advahov <evghenii.88@gmail.com>
2023-11-11 22:20:22 +0200

    Could add to Db

--- src/main/java/com/learn/blog/controllers/BlogController.java
+++ src/main/java/com/learn/blog/controllers/BlogController.java
@@ -6,13 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BlogController {
 
     @Autowired
     private PostRepository postRepository;
-
 
     @GetMapping("/blog")
     public String blogMain(Model model) {
@@ -21,5 +22,21 @@ public class BlogController {
         model.addAttribute("posts", posts);
         return "blog-main";
     }
+
+    @GetMapping("/article_add")
+    public String blogAdd(Model model) {
+        model.addAttribute("title", "Article add page");
+        return "article_add";
+    }
+
+    @PostMapping("/article_add")
+    public String addArticle(@RequestParam String title,
+                             @RequestParam String anons,
+                             @RequestParam String full_text, Model model) {
+        Post post = new Post(title, anons, full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
+
 }
 
