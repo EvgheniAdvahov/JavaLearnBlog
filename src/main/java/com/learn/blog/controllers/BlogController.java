d9b9923dd12f5cae5446a9270c46a4694eb5c9b8
Evgheni Advahov <evghenii.88@gmail.com>
2023-11-12 18:47:37 +0200

    course finished. mysql autoincriment issue

--- src/main/java/com/learn/blog/controllers/BlogController.java
+++ src/main/java/com/learn/blog/controllers/BlogController.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class BlogController {
@@ -30,11 +34,53 @@ public class BlogController {
     }
 
     @PostMapping("/article_add")
-    public String addArticle(@RequestParam String title,
+    public String postAdd(@RequestParam String title,
                              @RequestParam String anons,
                              @RequestParam String full_text, Model model) {
         Post post = new Post(title, anons, full_text);
         postRepository.save(post);
+        return "redirect:/blog";
+    }
+
+    @GetMapping("/blog-details/{id}")
+    public String postView(@PathVariable(value = "id") Long postId, Model model) {
+        if(!postRepository.existsById(postId)){
+            return "redirect:/blog";
+        }
+        Optional<Post> post = postRepository.findById(postId);
+        ArrayList<Post> result = new ArrayList<>();
+        post.ifPresent(result::add);
+        model.addAttribute("post", result);
+        return "blog-details";
+    }
+    @GetMapping("/blog-details/{id}/edit")
+    public String postEdit(@PathVariable(value = "id") Long postId, Model model) {
+        if(!postRepository.existsById(postId)){
+            return "redirect:/blog";
+        }
+        Optional<Post> post = postRepository.findById(postId);
+        ArrayList<Post> result = new ArrayList<>();
+        post.ifPresent(result::add);
+        model.addAttribute("post", result);
+        return "blog-edit";
+    }
+
+    @PostMapping("/blog-details/{id}/edit")
+    public String postRemove(@PathVariable(value = "id") Long postId,
+                             @RequestParam String title,
+                             @RequestParam String anons,
+                             @RequestParam String full_text, Model model) {
+        Post post = postRepository.findById(postId).orElseThrow();
+        post.setTitle(title);
+        post.setAnons(anons);
+        post.setFull_text(full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
+    @PostMapping("/blog-details/{id}/remove")
+    public String postRemove(@PathVariable(value = "id") Long postId, Model model) {
+        Post post = postRepository.findById(postId).orElseThrow();
+        postRepository.delete(post);
         return "redirect:/blog";
     }
 
